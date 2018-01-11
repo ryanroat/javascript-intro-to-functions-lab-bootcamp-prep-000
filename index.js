87e6344107aa52aead800a463db9949818de5752
Ryan Roat <ryanroat@gmail.com>
2018-01-11 18:05:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if string == string.toLowerCase() {
-    return "I can't hear you!"}
-    else if string == string.toUpperCase() {
-      return
+    return "I can't hear you!"
+    
+  }
+  else if string == string.toUpperCase() {
+    return "YES INDEED!"
+  }
+  else if string == 
+      
     }
   }
 }
